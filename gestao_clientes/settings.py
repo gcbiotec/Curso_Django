417e1df4cdf06367bde97eaefbf21146226f83e0
gabriel.silva <gcbiotec@gmail.com>
2021-08-27 10:35:39 -0300

    make it better

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # DATABASES = {
 #     'default': {
