04318198e12c06870a9e6bb8c0e1dc2f35634d26
gabriel.silva <gcbiotec@gmail.com>
2021-08-27 12:11:01 -0300

    Arrumando urls

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home, my_logout
 
 # Deve incluir /person/.. para acessar esta parte:
+
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', my_logout, name='my_logout'),
